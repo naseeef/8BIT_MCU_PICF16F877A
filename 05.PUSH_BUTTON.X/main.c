82d73027037f9f3523d3383d6219fe271e1260f3
Naseef A K <naseeefak@gmail.com>
2022-05-13 17:04:51 +0530

    Add files via upload

---
+++ 05.PUSH_BUTTON.X/main.c
@@ -0,0 +1,37 @@
+/*
+ * File:   main.c
+ * Author: naseef
+ *
+ * Created on 12 April, 2022, 8:13 PM
+ */
+// CONFIG
+#pragma config FOSC = HS        // Oscillator Selection bits (HS oscillator)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config BOREN = ON       // Brown-out Reset Enable bit (BOR enabled)
+#pragma config LVP = ON         // Low-Voltage (Single-Supply) In-Circuit Serial Programming Enable bit (RB3/PGM pin has PGM function; low-voltage programming enabled)
+#pragma config CPD = OFF        // Data EEPROM Memory Code Protection bit (Data EEPROM code protection off)
+#pragma config WRT = OFF        // Flash Program Memory Write Enable bits (Write protection off; all program memory may be written to by EECON control)
+#pragma config CP = OFF         // Flash Program Memory Code Protection bit (Code protection off)
+
+#include <xc.h>
+#define _XTAL_FREQ 16000000
+
+void main(void) {
+    
+    TRISB=0x00; //Is equal to 0x00
+    TRISD=0b00000001;
+    while(1)
+    {
+        if (RD0 == 1)
+        {
+        PORTB = 0x0f;
+        }
+        else
+        {
+            PORTB = 0;
+        }
+
+    }
+    return;
+}
