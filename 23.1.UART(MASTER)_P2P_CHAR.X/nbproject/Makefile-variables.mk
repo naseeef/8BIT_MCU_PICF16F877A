d90350e833f963ed1052b3a65c13fbe0f7bdfbf7
Naseef A K <naseeefak@gmail.com>
2022-05-13 17:15:50 +0530

    Add files via upload

---
+++ 23.1.UART(MASTER)_P2P_CHAR.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=23.1.UART_MASTER__P2P_CHAR.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/23.1.UART_MASTER__P2P_CHAR.X.production.hex
